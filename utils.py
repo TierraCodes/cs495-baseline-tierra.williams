42a8d1ae3a04b1dd8914a01e689299d2dc81b663
TierraCodes <tdwilliams2004@gmail.com>
2025-08-27 00:18:50 -0400

    refactored variable names

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 
-def palindrome_checker(word):
-    word = word.lower()
-    for x in range(len(word)//2):
-        if word[x] != word[len(word)-1-x]:
-            print(word + " is not a palindrome")
+def palindrome_checker(string):
+    string = string.lower()
+    for x in range(len(string)//2):
+        if string[x] != string[len(string)-1-x]:
+            print(string + " is not a palindrome")
             return False
-    print(word + " is a palindrome")
+    print(string + " is a palindrome")
     return True
         
